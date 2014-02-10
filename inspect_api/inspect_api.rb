6000057c922fa8c57a43275d05d287318800799c
Inderpal Singh <inderps@thoughtworks.com>
2014-02-10 15:38:26 +0530

    +pallavi added twitter api

--- inspect_api/inspect_api.rb
+++ inspect_api/inspect_api.rb
@@ -5,6 +5,7 @@ require 'sinatra/cross_origin'
 require 'json'
 require 'thin'
 require 'databasedotcom'
+require 'twitter'
 
 Bundler.require
 
@@ -12,15 +13,28 @@ configure do
   enable :cross_origin
 end
 
-post '/data' do
+twitter_client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "xye1a5G4BKZe8Y0pKNtq2w"
+  config.consumer_secret     = "3AJK7IAy6efTJ60ON3gdxC11JGcWhkfTQJdFiKudxc"
+  config.access_token        = "2336400037-1VAsykouIrUlafSxt3UKDklpcrvvzKkT5WCYOBH"
+  config.access_token_secret = "XHqA1Qoo8qfEgb3gTYPeInr9oN7HcQBBkOlSqHImq9yRr"
+end
+
+salesforce_client = Databasedotcom::Client.new(:host => "ap1.salesforce.com",
+                                               client_id: "3MVG9Y6d_Btp4xp4VMNknjtZ.dNfLj.keuAPeOHX7BBs7jkQl2veFyeBT1VpTDNbjB.jz12zKoVvrBoOboOI0",
+                                               client_secret: "2714833726334434871")
+salesforce_client.authenticate :username => "salesforce.ips@gmail.com", :password => "salesforce1!4Vcz4QKBkvtzbDNgC2x6GA9C5"
+
+post '/salesforce_post' do
   content_type :json
-  if(params[:end_point] == "salesforce")
-    salesforce_client = Databasedotcom::Client.new(:host => "ap1.salesforce.com",
-                                                   client_id: "3MVG9Y6d_Btp4xp4VMNknjtZ.dNfLj.keuAPeOHX7BBs7jkQl2veFyeBT1VpTDNbjB.jz12zKoVvrBoOboOI0",
-                                                   client_secret: "2714833726334434871")
-    salesforce_client.authenticate :username => "salesforce.ips@gmail.com", :password => "salesforce1!4Vcz4QKBkvtzbDNgC2x6GA9C5"
-    Temperature = salesforce_client.materialize("Temperature__c")
-    Temperature.create(reading__c: params[:reading], created_at__c: Time.now)
-  end
+
+  Temperature = salesforce_client.materialize("Temperature__c")
+  Temperature.create(reading__c: params[:reading], created_at__c: Time.now)
+  {message: "Done!!"}.to_json
+end
+
+post '/twitter_post' do
+  content_type :json
+  twitter_client.update("tweeted reading: #{params[:reading]} at #{params[:created_at]}")
   {message: "Done!!"}.to_json
 end
--- mobile/app/app.js
+++ mobile/app/app.js
@@ -1,17 +1,8 @@
 $(document).ready(function(){
     $("#sendData").click(function(event){
         event.preventDefault();
-        $.ajax({
-            url: "http://localhost:3000/data",
-            type: "POST",
-            data: {
-                reading: "45",
-                end_point: "salesforce"
-            },
-            dataType: "json",
-            success: function(result){
-                alert(result.message);
-            }
-        })
+        TwitterEndpoint.send("45", new Date(), function(result){
+            alert(result.message);
+        });
     });
 });
